6b573039a8932a8231b9b375f173df4d9508ac4c
Kazimir <avvlasov@outlook.com>
2021-11-22 11:54:54 +0300

    Добавил Controls.UniversalDataGrid

---
+++ Controls/UniversalDataGrid/UniversalDataGridAuto.xaml.cs
@@ -0,0 +1,39 @@
+﻿using DataGridTextColumn = System.Windows.Controls.DataGridTextColumn;
+
+namespace MyServicesLibrary.Controls.UniversalDataGrid;
+public partial class UniversalDataGrid : UserControl
+{
+    private List<DataGridStruct> tableStruct;
+    public UniversalDataGrid(List<DataGridStruct> tableStructure)
+    {
+        tableStruct = tableStructure;
+        InitializeComponent();
+        myDataGrid.AutoGeneratingColumn += myDataGrid_AutoGeneratingColumn;
+    }
+    public void Show<T>(List<T> tableData)
+    {
+        myDataGrid.ItemsSource = tableData;
+    }
+    void myDataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+    {
+        DataGridStruct rec = tableStruct.Find(m => m.Binding == e.PropertyName);
+        if (rec == null)
+            e.Cancel = true;
+        else
+        {
+            e.Column.Header = rec.Headers;
+            e.Column.Width = new DataGridLength(rec.ColWidth, DataGridLengthUnitType.Star);
+            if (e.PropertyType != typeof(double))
+            {
+                (e.Column).HeaderStyle = this.Resources["mdDataGridTextColumnHeaderStyleLeft"] as Style;
+            }
+            if (e.PropertyType == typeof(double))
+            {
+                (e.Column as DataGridTextColumn).Binding.StringFormat = rec.NumericFormat;
+                (e.Column as DataGridTextColumn).ElementStyle = this.Resources["mdDataGridTextColumnStyle"] as Style;
+                (e.Column).HeaderStyle = this.Resources["mdDataGridTextColumnHeaderStyleRight"] as Style;
+            }
+
+        }
+    }
+}
